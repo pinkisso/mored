6e224a8187e636bf4a11793990a874307e89ea57
pinkisso <pink34074@gmail.com>
2026-05-23 05:27:50 +0200

    Update cmt.py

--- res/26-1/cmt.py
+++ res/26-1/cmt.py
@@ -6,11 +6,5 @@ url = "https://tvradiozap.eu/tools/m3u-m3u8.php/para/TMC.m3u8"
 response = requests.get(url, allow_redirects=True)
 response.raise_for_status()
 
-# Print final redirected URL
-print("Redirected to:")
-print(response.url)
-
-print("\nContent:\n")
-
-# Print content of redirected .m3u8 file
+# Print only the content of the redirected .m3u8 file
 print(response.text)
