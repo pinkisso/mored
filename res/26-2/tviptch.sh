b7f62013abcd64e81ab181d7d173775bce5a8093
pinkisso <pink34074@gmail.com>
2026-06-17 17:31:42 +0200

    Create tviptch.sh

---
+++ res/26-2/tviptch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file1="res/26-2/cnnpt.m3u8"
+file2="res/26-2/tvi.m3u8"
+file3="res/26-2/tviint.m3u8"
+file4="res/26-2/tvific.m3u8"
+file5="res/26-2/tvimai.m3u8"
+file6="res/26-2/tvirea.m3u8"
+
+for file in "$file1" "$file2" "$file3" "$file4" "$file5" "$file6"; do
+  sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "$file"
+done
+exit 0
