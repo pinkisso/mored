5d29d12fafd63b499469f4a27a39860172e8e0f8
pinkisso <pink34074@gmail.com>
2026-06-17 17:34:21 +0200

    Update tviptch.sh

--- res/26-2/tviptch.sh
+++ res/26-2/tviptch.sh
@@ -6,8 +6,9 @@ file3="res/26-2/tviint.m3u8"
 file4="res/26-2/tvific.m3u8"
 file5="res/26-2/tvimai.m3u8"
 file6="res/26-2/tvirea.m3u8"
+file7="res/26-2/tviafr.m3u8"
 
-for file in "$file1" "$file2" "$file3" "$file4" "$file5" "$file6"; do
+for file in "$file1" "$file2" "$file3" "$file4" "$file5" "$file6" "$file7"; do
   sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" "$file"
 done
 exit 0
