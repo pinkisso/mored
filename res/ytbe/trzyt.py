c8847dd91c36e411f68b87764a40132abdbec42d
pinkisso <pink34074@gmail.com>
2026-07-21 05:40:58 +0300

    Update trzyt.py

--- res/ytbe/trzyt.py
+++ res/ytbe/trzyt.py
@@ -11,7 +11,7 @@ playlists = {
     "lemedia.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x8dtuir.m3u8",
 	"7alimoges.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x5gv5v0.m3u8",
 	"moselle.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x9lgbik.m3u8",
-	#"tv8mb.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x3wqv8b.m3u8",
+	"tv8mb.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x3wqv8b.m3u8",
 	"angers.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x8r7256.m3u8",
 	"lyoncap.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/dm/x7z3zqa.m3u8",
 	"szcytbe.m3u8" : "https://tvradiozap.eu/tools/cof-m3u8.php/yt/UCOulx_rep5O4i9y6AyDqVvw.m3u8",
