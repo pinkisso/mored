a0c474481d97b2ba6232d07d7b57e6301bbcfdc0
pinkisso <pink34074@gmail.com>
2026-08-03 10:33:32 +0300

    Update n12.sh

--- res/isr/n12.sh
+++ res/isr/n12.sh
@@ -13,16 +13,20 @@ if [ ! -d "$output_dir" ]; then
     exit 1
 fi
 
-n12_content=$(cat "$input_file" | sed 's/direct/n12/g; s/2033791/2103938/g')
-n12b_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#n12/hls/live/2041434/n12_b#g; s/_2200/_850/g')
+n12_content=$(cat "$input_file" | sed 's/stream/n12/g; s/2033791/2103938/g')
+n12b_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#n12/hls/live/2041434/n12_b#g; s/_2200/_850/g')
 k12cc_content=$(cat "$input_file" | sed 's/k12/k12cc/g; s/2033791/2035325/g')
-ch24_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#direct/hls/live/2035340/ch24live#g; s/index_2200/video_7201280_p_1/g')
-erets_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#free/hls/live/2111419/erets/#g; s/index_2200/video_7201280_p_1/g')
-savri_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#free/hls/live/2111419/savri/#g; s/index_2200/video_7201280_p_1/g')
-hatuna_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#free/hls/live/2111419/hatuna/#g; s/index_2200/video_7201280_p_1/g')
-kohav_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#free/hls/live/2111419/kohav/#g; s/index_2200/video_7201280_p_1/g')
-ninja_content=$(cat "$input_file" | sed 's#direct/hls/live/2033791/k12#free/hls/live/2111419/ninja/#g; s/index_2200/video_7201280_p_1/g')
+ch24_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#direct/hls/live/2035340/ch24live#g; s/index_2200/video_7201280_p_1/g')
+erets_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#free/hls/live/2111419/erets/#g; s/index_2200/video_7201280_p_1/g')
+savri_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#free/hls/live/2111419/savri/#g; s/index_2200/video_7201280_p_1/g')
+hatuna_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#free/hls/live/2111419/hatuna/#g; s/index_2200/video_7201280_p_1/g')
+kohav_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#free/hls/live/2111419/kohav/#g; s/index_2200/video_7201280_p_1/g')
+ninja_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#free/hls/live/2111419/ninja/#g; s/index_2200/video_7201280_p_1/g')
+k12wad_content=$(cat "$input_file" | sed 's/k12/k12n12wad/g')
+k12rh_content=$(cat "$input_file" | sed 's#stream/hls/live/2033791/k12#n12/hls/live/20000821/k12rh#g')
 
+echo "$k12wad_content" > "$output_dir/k12wad.m3u8"
+echo "$k12rh_content" > "$output_dir/k12rh.m3u8"
 echo "$n12_content" > "$output_dir/n12.m3u8"
 echo "$n12b_content" > "$output_dir/n12b.m3u8"
 echo "$k12cc_content" > "$output_dir/k12cc.m3u8"
